e7ebb58b027eb1dc40492c1ef9af252695f75f51
Lee Yuna <leeyuna7689@gmail.com>
2026-04-02 23:33:47 +0900

    feat: Auth.js에 MongoDB adapter 연동

--- auth.ts
+++ auth.ts
@@ -1,8 +1,11 @@
+import client from '@/lib/db';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import NextAuth from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: MongoDBAdapter(client),
   providers: [
     GoogleProvider({
       clientId: process.env.AUTH_GOOGLE_ID as string,
