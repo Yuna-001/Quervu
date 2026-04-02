e4fe1bbcbabdb569c89e254e77cc3ee25eaaf898
Lee Yuna <leeyuna7689@gmail.com>
2026-04-02 23:18:26 +0900

    feat: Auth.js 기본 설정 및 OAuth provider 추가

---
+++ auth.ts
@@ -0,0 +1,16 @@
+import NextAuth from 'next-auth';
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID as string,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET as string,
+    }),
+    GithubProvider({
+      clientId: process.env.AUTH_GITHUB_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+    }),
+  ],
+});
