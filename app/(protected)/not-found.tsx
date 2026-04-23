a60a298ae935cf2d4119e8d5ef290c32c8bee324
Lee Yuna <leeyuna7689@gmail.com>
2026-04-23 22:45:44 +0900

    chore: openai 설치

---
+++ app/(protected)/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <h1 className="flex items-center justify-center min-h-[calc(100vh-300px)] text-center text-slate-600 dark:text-slate-300">
+      해당 페이지를 찾을 수 없습니다.
+    </h1>
+  );
+}
