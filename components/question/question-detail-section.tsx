a2996159903933e5bf4260a92514e799b22051e8
Lee Yuna <leeyuna7689@gmail.com>
2026-05-02 01:10:47 +0900

    feat: 모범 답변 영역을 Accordion UI로 구성
    - QuestionDetailSection에서 모범 답변 영역을 접고 펼칠 수 있도록 구성

--- components/question/question-detail-section.tsx
+++ components/question/question-detail-section.tsx
@@ -1,4 +1,10 @@
 import { TagList } from '@/components/common/tag-list';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 type QuestionDetailSectionProps = {
   question: string;
@@ -18,10 +24,18 @@ export function QuestionDetailSection({
         <h1 className="text-base">{question}</h1>
       </div>
       <TagList tags={tags} />
-      <div>
-        <h2>모범 답변</h2>
-        <p>{idealAnswer}</p>
-      </div>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-1">
+          <div className="flex items-center justify-start">
+            <AccordionTrigger className="px-2 py-2.5 cursor-pointer hover:no-underline gap-2 -ml-1">
+              모범 답변
+            </AccordionTrigger>
+          </div>
+          <AccordionContent className="rounded-md p-4 bg-gray-200 dark:bg-gray-700">
+            {idealAnswer}
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </section>
   );
 }
